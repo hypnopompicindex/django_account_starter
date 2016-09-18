af046193ff6ef786ce8329539f3f7c6c6d129645
hypnopompicindex <hypnopompicindex@gmail.com>
2016-09-18 11:05:02 -0400

    added logout view & templates

--- example/django_account_starter/urls.py
+++ example/django_account_starter/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^login/$', views.user_login, name='login'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout', name='logout'),
+    url(r'^logout-then-login/$', 'django.contrib.auth.views.logout_then_login', name='logout_then_login'),
 ]
